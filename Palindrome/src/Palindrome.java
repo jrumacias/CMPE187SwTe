d6f5bedafff661f880811f268a26eebae46d551e
jrumacias <juan@jrmacias.com>
2019-03-08 17:19:21 -0800

    Add Palindrome class/object functionality

--- Palindrome/src/Palindrome.java
+++ Palindrome/src/Palindrome.java
@@ -1,24 +1,35 @@
 public class Palindrome {
+    private boolean isPalindrome;
+    private int length;
+    private String phrase;
 
-    public static boolean palindromeCheck(String s) {
+    public Palindrome(String phrase) {
+        this.isPalindrome = true;
+        this.length = phrase.replaceAll("[^a-zA-Z0-9]", "").toLowerCase().length();
+        this.phrase = phrase;
+    }
+
+    public static void palindromeCheck(Palindrome p) {
         // Remove non alphanumeric characters from string and convert to lowercase
-        s = s.replaceAll("[^a-zA-Z0-9]", "");
-        s = s.toLowerCase();
+        p.phrase = p.phrase.replaceAll("[^a-zA-Z0-9]", "");
+        p.phrase = p.phrase.toLowerCase();
 
         int p1 = 0;
-        int p2 = s.length()-1;
+        int p2 = p.phrase.length()-1;
 
         while (p1 < p2) {
-            if (s.charAt(p1) != s.charAt(p2)) {
-                return false;
+            if (p.phrase.charAt(p1) != p.phrase.charAt(p2)) {
+                p.isPalindrome =  false;
             }
             p1++;
             p2--;
         }
-        return true;
     }
 
     public static void main(String[] args) {
-        System.out.println(palindromeCheck("0P"));
+        Palindrome testPalindrome = new Palindrome("A man, a plan, a canal: Panama");
+
+        System.out.println(testPalindrome.isPalindrome);
+        System.out.println(testPalindrome.length);
     }
 }
