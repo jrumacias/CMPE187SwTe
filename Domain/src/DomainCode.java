4b3f673423b99659ee6ec1e982f44e34cff38f3b
jrumacias <juan@jrmacias.com>
2019-03-17 16:20:31 -0700

    update juanDev branch with most up-to-date code

--- Domain/src/DomainCode.java
+++ Domain/src/DomainCode.java
@@ -1,21 +1,39 @@
+/**
+ * Domain Testing
+ *
+ * Original code written by: Juan Macias
+ * San Jose State University
+ * Spring 2019
+ * CMPE187: Software Testing
+ *
+ * NOTES
+ *  The following code:
+ *      - Only accepts integer values for x, y inputs
+ *      - Does not implement input validation
+ */
+
 public class DomainCode {
 
-    public static int codeDomain(int x, int y){
+    public static int codeDomain(int x, int y) {
+//        1. Initialize variables
         int c, d, k;
+//        2. Assignment
         c = (x * x) - y;
-        if(c > 25) {                //OFF point closed
+//        3. P1
+        if(c > 25) {
+//            4. Branch P1 = True
             d = c - (x / 4);
-        } else d = c + (x / 4);     //OFF point open
-
-        if(d >= y * 2) {            //ON point closed
+        } else d = c + (x / 4); // 5. Branch P1 = False
+//        6. P2
+        if(d >= c - 3) {
+//            7. Branch P2 = True
             k = x + (d * 2);
-        } else k = y + (d / 4);     //ON point open
-
+        } else k = y + (d / 4); // 8. Branch P2 = False
+//        9. Return Statement
         return(k);
     }
 
     public static void main(String[] args) {
         System.out.println(codeDomain(5, 2));
-        System.out.println(26/4);
     }
 }
