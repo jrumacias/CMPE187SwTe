eedc7e0a06a0a24627a762a9744574436c09ddab
JessicaHoang <32215166+JessicaHoang@users.noreply.github.com>
2019-03-16 23:37:24 -0700

    Update DomainCode.java

--- Domain/src/DomainCode.java
+++ Domain/src/DomainCode.java
@@ -3,13 +3,13 @@ public class DomainCode {
     public static int codeDomain(int x, int y){
         int c, d, k;
         c = x * x;
-        if(c > 25) {
+        if(c > 25) {                //OFF point closed
             d = c - (x / 4);
-        } else d = c + (x / 4);
+        } else d = c + (x / 4);     //OFF point open
 
-        if(d >= y * 2) {
+        if(d >= y * 2) {            //ON point closed
             k = x + (d * 2);
-        } else k = y + (d / 4);
+        } else k = y + (d / 4);     //ON point open
 
         return(k);
     }
