4e9d823a9d8c19b3d1f8c2288e72a7872aefe73e
jrumacias <juan@jrmacias.com>
2019-03-17 20:19:18 -0700

    update P1 and P2

--- Domain/src/DomainCode.java
+++ Domain/src/DomainCode.java
@@ -17,8 +17,10 @@ public class DomainCode {
     public static int codeDomain(int x, int y) {
 //        1. Initialize variables
         int c, d, k;
+        System.out.println("Testing Point: (" + x + ", " + y + ")");
 //        2. Assignment
         c = (x * x) - y;
+        System.out.println("x^2 - y = c = " + c);
 //        3. P1
         if(c > 25) {
 //            4. Branch P1 = True
@@ -26,23 +28,24 @@ public class DomainCode {
             System.out.println("P1: True");
         }
         else {
-            d = c + (x / 3); // 5. Branch P1 = False
+            d = c + (x / 4); // 5. Branch P1 = False
             System.out.println("P1: False");
         }
 //        6. P2
+        System.out.println("d = " + d);
         if(d >= c + 3) {
 //            7. Branch P2 = True
             k = x + (d * 2);
             System.out.println("P2: True");
         } else {
             k = y + (d / 4); // 8. Branch P2 = False
-            System.out.println("P2 False");
+            System.out.println("P2: False");
         }
 //        9. Return Statement
         return(k);
     }
 
     public static void main(String[] args) {
-        System.out.println(codeDomain(0, -50));
+        System.out.println("k = " + codeDomain(-15, 300));
     }
 }
